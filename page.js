1dc1f9456f78b22e5933ce8b4a8c512215f916c6
RenanBick <renanbickdev@gmail.com>
2024-03-21 11:42:26 -0300

    additions

--- page.js
+++ page.js
@@ -1,36 +1,25 @@
-import { degrees, PDFDocument, rgb, StandardFonts } from 'pdf-lib';
 
-const canvas = document.querySelector('canvas');
+const titulo = document.getElementById('tituloTrab');
+const aluno = document.getElementById('nomeAluno');
+const orientador = document.getElementById('nomeProfOr');
+const avaliador1 = document.getElementById('nomeProfAv1');
+const avaliador2 = document.getElementById('nomeProfAv2');
+const curso = document.getElementById('curso');
+const nota = document.getElementById('nota');
+const data_hora = document.getElementById('data-hora');
+const data_fim = document.getElementById('dataFim');
+const sala = document.getElementById('sala');
+const assinaturaOr = document.getElementById('sala');
+const assinaturaAv1 = document.getElementById('sala');
+const assinaturaAv2 = document.getElementById('sala');
 
-const form = document.querySelector('.signature-pad-form');
+function gerarPDF() {
+    // Crie um novo documento PDF
+    const doc = new jsPDF();
 
+    // Adicione o nome ao PDF
+    doc.text(`Nome: ${aluno}`, 10, 10);
 
-const titulo = document.getElementById('tituloTrab').value;
-const aluno = document.getElementById('nomeAluno').value;
-const orientador = document.getElementById('nomeProfOr').value;
-const avaliador1 = document.getElementById('nomeProfAv1').value;
-const avaliador2 = document.getElementById('nomeProfAv2').value;
-const curso = document.getElementById('curso').value;
-const nota = document.getElementById('nota').value;
-const data_hora = document.getElementById('data-hora').value;
-const data_fim = document.getElementById('dataFim').value;
-const sala = document.getElementById('sala').value;
-const assinaturaOr = document.getElementById('sala').value;
-const assinaturaAv1 = document.getElementById('sala').value;
-const assinaturaAv2 = document.getElementById('sala').value;
-
-
-async function modifyPdf() {
-    const url = 'https://pdf-lib.js.org/assets/with_update_sections.pdf'
-    const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer())
-
-    const pdfDoc = await PDFDocument.load(existingPdfBytes)
-    const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica)
-
-    const pages = pdfDoc.getPages()
-    const firstPage = pages[0]
-    const { width, height } = firstPage.getSize()
-
-
-    const pdfBytes = await pdfDoc.save()
+    // Salve o PDF (ou ofereça para download)
+    doc.save('formulario.pdf');
 }
